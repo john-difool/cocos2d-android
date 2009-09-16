1cda45e6f29b2d10319d77d5f7d3459e8ff2fad7
zhuangtaizzt <zhuangtaizzt@2ac3bcc4-a19b-11de-ba4d-5dbaeb624ccf>
2009-09-16 05:16:08 +0000

    remove R.java
    git-svn-id: http://cocos2d-android.googlecode.com/svn/trunk@3 2ac3bcc4-a19b-11de-ba4d-5dbaeb624ccf

--- src/com/moandroid/cocos2d/nodes/CocosNode.java
+++ src/com/moandroid/cocos2d/nodes/CocosNode.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.graphics.PointF;
+import android.view.MotionEvent;
 
 import com.moandroid.cocos2d.Camera;
+import com.moandroid.cocos2d.action.Action;
 import com.moandroid.cocos2d.effects.GridBase;
+import com.moandroid.cocos2d.types.AffineTransform;
+import com.moandroid.cocos2d.types.ccSelector;
+import com.moandroid.cocos2d.types.ccSize;
+import com.moandroid.cocos2d.types.ccTime;
 
 public class CocosNode {
 	public float rotation;
@@ -60,5 +66,145 @@ public class CocosNode {
 	
 	public CocosNode addChild(CocosNode node, int z, PointF parallaxRatio){
 		return null;
+	}
+	
+	public void removeChild(CocosNode node, boolean cleanup){
+		
+	}
+	
+	public void removeChildrenWithCleanup(boolean cleanup){
+		
+	}
+	
+	public CocosNode getChildByTag(int tag){
+		return null;
+	}
+	
+	public void reorderChild(CocosNode child,int zOrder){
+		
+	}
+	
+	public void draw(){
+		
+	}
+	
+	public void visit(){
+		
+	}
+	
+	public void transform(){
+		
+	}
+	
+	public void transformAncestors(){
+		
+	}
+	
+	public Action runAction(Action action){
+		return null;
+	}
+	
+	public void stopAllActions(){
+		
+	}
+	
+	public void stopAction(Action action){
+		
+	}
+	
+	public void stopActionByTag(int tag){
+		
+	}
+	
+	public Action getActionByTag(int tag){
+		return null;
+	}
+	
+	public int numberOfRunningActions(){
+		return 0;
+	}
+	
+	public void schedule(ccSelector s){
+		
+	}
+	
+	public void schedual(ccSelector s, ccTime seconds){
+		
+	}
+	
+	public void unschedule(ccSelector s){
+		
+	}
+	
+	public AffineTransform nodeToWorldTransform(){
+		return null;
+	}
+	
+	public AffineTransform worldToNodeTransform(){
+		return null;
+	}
+	
+	public PointF convertToNodeSpace(PointF worldPoint){
+		return null;
+	}
+	
+	public PointF convertToWorldSpace(PointF nodePoint){
+		return null;
+	}
+	
+	public PointF convertToWorldSpaceAR(PointF nodePoint){
+		return null;
+	}
+	
+	public PointF convertTouchToNodeSpace(MotionEvent event){
+		return null;
+	}
+	
+	public PointF convertTouchToNodeSpaceAR(MotionEvent event){
+		return null;
+	}
+	
+	public interface CocosNodeOpacity {
+		public byte opacity();
+		public void setOpacity(byte opacity);
+	}
+	
+	public interface CocosNodeSize{
+		public ccSize contentSize();
+	}
+	
+	public interface CocosNodeRGB{
+		public void setRGB(byte r, byte g, byte b);
+		public byte r();
+		public byte g();
+		public byte b();
+	}
+	
+	public interface CocosAnimation{
+		public ArrayList<Object> frames();
+		public float delay();
+		public String name();
+	}
+	
+	public interface CocosNodeFrames{
+		public void setDisplayFrame(Object newFrame);
+		public void setDisplayFrame(String animationName, int frameIndex);
+		public boolean isFrameDisplayed(Object frame);
+		public Object displayFrame();
+		public CocosAnimation animationByName(String animationName);
+		public void addAnimation(CocosAnimation animation);
+		public void setAutoCenterFrames(boolean autoCenterFrames);
+	}
+	
+	private void step_(ccTime dt){
+		
+	}
+	
+	private void activateTimers(){
+		
+	}
+	
+	private void deactivateTimers(){
+		
 	}
 }
