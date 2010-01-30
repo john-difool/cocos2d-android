981587c88c0fa614bd2fc5475c12cf13826cdfd1
CardillosCreations <CardillosCreations@2ac3bcc4-a19b-11de-ba4d-5dbaeb624ccf>
2010-01-30 19:08:24 +0000

    Added first implementation for accelerometer in Layer.  A bit different than on the objective-c/iPhone version because on that platform the accelerometer events have a well defined method that must be implemented by the specific Layer subclass that wants the event.  On the Java/android platform, a Sensor listener is required for the active activity.
    git-svn-id: http://cocos2d-android.googlecode.com/svn/trunk@74 2ac3bcc4-a19b-11de-ba4d-5dbaeb624ccf

--- src/org/cocos2d/layers/Layer.java
+++ src/org/cocos2d/layers/Layer.java
@@ -1,19 +1,30 @@
 package org.cocos2d.layers;
 
-import android.view.MotionEvent;
+import org.cocos2d.events.TouchDelegate;
+import org.cocos2d.events.TouchDispatcher;
 import org.cocos2d.nodes.CocosNode;
 import org.cocos2d.nodes.Director;
-import org.cocos2d.events.TouchDispatcher;
-import org.cocos2d.events.TouchDelegate;
 import org.cocos2d.types.CCSize;
 
-public class Layer extends CocosNode implements TouchDelegate {
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+import android.view.MotionEvent;
 
+public class Layer extends CocosNode implements TouchDelegate, SensorEventListener {
+	
+    // used to control registration of Touch events
     protected boolean isTouchEnabled_;
 
-    //! whether or not it will receive Accelerometer events
+    // used to control registration of Accelerometer events
     protected boolean isAccelerometerEnabled_;
 
+    protected final SensorManager sensorManager;
+    protected final Sensor accelerometer;
+    
     public boolean isTouchEnabled()
     {
         return isTouchEnabled_;
@@ -32,11 +43,38 @@ public class Layer extends CocosNode implements TouchDelegate {
         }
     }
 
+    public boolean isAccelerometerEnabled()
+    {
+        return isAccelerometerEnabled_;
+    }
+
+    public void setIsAccelerometerEnabled(boolean enabled)
+    {
+        if( isAccelerometerEnabled_ != enabled ) {
+        	isAccelerometerEnabled_ = enabled;
+            if( isRunning() ) {
+                if( enabled )
+                    registerWithAccelerometer();
+                else
+                    unregisterWithAccelerometer();
+            }
+        }
+    }
+    
     public static Layer node() {
         return new Layer();
     }
 
     protected Layer() {
+    	// get the SensorManager from the Activity
+    	sensorManager = (SensorManager) Director.sharedDirector().getActivity().getSystemService(Context.SENSOR_SERVICE);
+    	
+    	// if we have a SensorManager then get the accelerometer Sensor
+    	if (sensorManager != null) 
+    		accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    	else
+    		accelerometer = null;
+    	
         CCSize s = Director.sharedDirector().winSize();
         setRelativeAnchorPoint(false);
 
@@ -46,13 +84,29 @@ public class Layer extends CocosNode implements TouchDelegate {
 
         isTouchEnabled_ = false;
         isAccelerometerEnabled_ = false;
-            
     }
 
     protected void registerWithTouchDispatcher() {
         TouchDispatcher.sharedDispatcher().addDelegate(this, 0);
     }
+    
+    protected void registerWithAccelerometer()
+    {
+    	if (accelerometer != null) {
+    		boolean registered = sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+    		if (!registered) {
+    	        Log.e("Layer", "Could not register accelerometer sensor listener!");
+    		}
+    	}
+    }
 
+    protected void unregisterWithAccelerometer()
+    {
+    	if (accelerometer != null) {
+    		sensorManager.unregisterListener(this, accelerometer);
+    	}
+    }
+    
     @Override
     public void onEnter() {
 
@@ -64,8 +118,8 @@ public class Layer extends CocosNode implements TouchDelegate {
         // then iterate over all the children
         super.onEnter();
 
-//        if( isAccelerometerEnabled )
-//            [[UIAccelerometer sharedAccelerometer] setDelegate:self];
+        if( isAccelerometerEnabled_ )
+        	registerWithAccelerometer();
     }
 
     @Override
@@ -74,8 +128,8 @@ public class Layer extends CocosNode implements TouchDelegate {
         if (isTouchEnabled_)
             TouchDispatcher.sharedDispatcher().removeDelegate(this);
 
-//        if( isAccelerometerEnabled )
-//            [[UIAccelerometer sharedAccelerometer] setDelegate:nil];
+        if( isAccelerometerEnabled_ )
+        	unregisterWithAccelerometer();
 
         super.onExit();
     }
@@ -95,4 +149,21 @@ public class Layer extends CocosNode implements TouchDelegate {
     public boolean ccTouchesCancelled(MotionEvent event) {
         return TouchDispatcher.kEventIgnored;  // TODO Auto-generated method stub
     }
+
+    public void ccAccelerometerChanged(float accelX, float accelY, float accelZ) {
+		// Override to process accelerometer events.
+    }
+    
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// Override to process sensor accuracy changes (for any registered sensors).
+	}
+
+	public void onSensorChanged(SensorEvent event) {
+		// Override to process other sensor change events.
+		// Make sure you this base implementation if you want accelerometer events passed.
+		
+		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			ccAccelerometerChanged(event.values[0], event.values[1], event.values[2]);
+		}
+	}
 }
